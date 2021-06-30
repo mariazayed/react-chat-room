6805c065d15f7d89277ac4f9cc46ca49f5d38843
Maria Zayed <m.zayed@bisan.com>
2021-06-30 08:36:08 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -20,8 +20,6 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-
 
 function App() {
 
@@ -30,7 +28,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>Group chat 🔥💬</h1>
         <SignOut />
       </header>
 
@@ -52,7 +50,6 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
   )
 
@@ -102,9 +99,10 @@ function ChatRoom() {
 
     <form onSubmit={sendMessage}>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)}
+             placeholder="Say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>Send</button>
 
     </form>
   </>)
